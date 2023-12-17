4fff5b779735d047bf8bfaf1fc510bda5c601a29
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-12-17 00:27:39 -0500

    format specifiers and the monster regex that makes them possible

--- source/dasfor.d
+++ source/dasfor.d
@@ -1,21 +1,79 @@
 module dasfor;
 import std.conv;
 import std.regex;
+import std.string;
 import std.typecons;
 
 auto dasformat(Args...)(string source, Args a)
 {
     auto result = "", 
-         pattern = `\$[0-9]+`, 
+         pattern = `\$[0-9]+(:([a-z][0-9]?)|([A-Z][0-9]?))?`, 
          splitted = source.splitter!(Yes.keepSeparators)(pattern.regex);
-         
-    string[] items;
-    foreach(item; a) items ~= item.to!string;
     
     foreach (s; splitted)
     {
         if (s.matchFirst(regex("^" ~ pattern ~ "$")))
-            result ~= items[s[1..$].to!uint];
+        {
+            auto markerSplitted = s.split(":"),
+                 index = s[1..$].to!uint,
+                 item = a[index].to!string;
+                 
+            if (markerSplitted.length == 2)
+                switch (markerSplitted[1])
+                {
+                    case "a", "A":
+                        item = "%a".format(a[index]);
+                        break;
+                        
+                    case "b":
+                        item = "%b".format(a[index]);
+                        break;
+                        
+                    case "c":
+                        item = "%c".format(a[index]);
+                        break;
+                        
+                    case "d":
+                        item = "%d".format(a[index]);
+                        break;
+                        
+                    case "e", "E":
+                        item = "%e".format(a[index]);
+                        break;
+                        
+                    case "f", "F":
+                        item = "%f".format(a[index]);
+                        break;
+                        
+                    case "g", "G":
+                        item = "%e".format(a[index]);
+                        break;
+                        
+                    case "o":
+                        item = "%o".format(a[index]);
+                        break;
+                        
+                    case "r":
+                        item = "%r".format(a[index]);
+                        break;
+                    
+                    case "s":
+                        item = "%s".format(a[index]);
+                        break;
+                        
+                    case "u":
+                        item = "%u".format(a[index]);
+                        break;
+                    
+                    case "x", "X":
+                        item = "%x".format(a[index]);
+                        break;
+                        
+                    default: // do nothing
+                }
+            
+            result ~= item;
+        }
         
         else result ~= s;
     }
